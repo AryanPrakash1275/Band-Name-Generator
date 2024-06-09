57e41d0019185877082be1a24416a7c7866eeaf6
Aryan Prakash <aryanprakash1275@gmail.com>
2024-06-09 13:55:20 +0530

    fixes

--- band-name-generator.py
+++ band-name-generator.py
@@ -1,9 +1,9 @@
 #main
 print('Welcome!')
-print('This gen is created by Aryan.')
+print('This gen is created by Aryan.\n')
 #quetion 1
 question1 = input('Which city did you grew up?\n')
 #question 2
 question2 = input('Whats your favourite mythical creature?\n')
 #combining
-print(question1 + question2)
+print("Your Band Name could be " + question1 +" "+ question2 + ".")
